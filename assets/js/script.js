0a5d9f306b0a8bb56b49072979954b6a7126428d
Tony <tonyguarino1489@gmail.com>
2023-03-31 15:03:12 -0400

    Started fetch with Youtube API

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,21 @@ $(function () {
         playMusicButton = $('#play-music'),
         musixMatchKey = '933db136de4c5690257d41e434ec1143',
         youtubeKey = 'AIzaSyBVQsjnNwpI-fOih0uJq-n1KCb1WJTvmh8';
+        youtubeURL = ``;
 
     // Button click event to search for tracks which also grabs the users input text value
     searchButton.click((e) => {
         e.preventDefault() 
         const inputValue = $('#input-value').val()
+        youtubeURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${inputValue}l&key=${youtubeKey}`
+
+        $.ajax({
+            url: youtubeURL,
+            method: 'GET',
+            dataType: 'json',
+            success: data => console.log(data),
+            error: err => console.error(err)
+        })
     })
-
-
-
 
 })

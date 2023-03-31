32d39c14acc36710d64893ee2419160641e4c9a5
Tony <tonyguarino1489@gmail.com>
2023-03-31 13:10:29 -0400

    added api keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 $(function () {
-    const searchButton = $('#search')
-    const lyricsText = $('#lyrics')
-    const playMusicButton = $('#play-music')
-    const youtubeAPI = 'AIzaSyBVQsjnNwpI-fOih0uJq-n1KCb1WJTvmh8'
+    const searchButton = $('#search');
+    const lyricsText = $('#lyrics');
+    const playMusicButton = $('#play-music');
+    const musixmatch_key = "933db136de4c5690257d41e434ec1143";
+    const youtube_key = "AIzaSyBVQsjnNwpI-fOih0uJq-n1KCb1WJTvmh8";
 
 })

907626d6f69b4a5bfebc0165d6297029d566a071
Tony <tonyguarino1489@gmail.com>
2023-03-31 14:31:16 -0400

    Updated input id on index and added js and jquery functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 $(function () {
-    const 
+    const
         searchButton = $('#search'),
         lyricsText = $('#lyrics'),
         playMusicButton = $('#play-music'),
-        inputValue = $('#input-value'),
         musixMatchKey = '933db136de4c5690257d41e434ec1143',
         youtubeKey = 'AIzaSyBVQsjnNwpI-fOih0uJq-n1KCb1WJTvmh8';
+
+    // Button click event to search for tracks which also grabs the users input text value
+    searchButton.click((e) => {
+        e.preventDefault() 
+        const inputValue = $('#input-value').val()
+    })
+
+
 
 
 })

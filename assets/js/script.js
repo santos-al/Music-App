c95e77f8cc178974c9c48f707a0f957d5db9e7b4
Tony <tonyguarino1489@gmail.com>
2023-03-31 12:55:57 -0400

    first js commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(function () {
+    const searchButton = $('#search')
+    const lyricsText = $('#lyrics')
+    const playMusicButton = $('#play-music')
+    const youtubeAPI = 'AIzaSyBVQsjnNwpI-fOih0uJq-n1KCb1WJTvmh8'
+
+})

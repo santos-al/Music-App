38d222d0ba7ac79b62bcedfadb741d756f5ac508
Tony <tonyguarino1489@gmail.com>
2023-03-31 13:11:52 -0400

    changed to camelcase

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ $(function () {
     const searchButton = $('#search');
     const lyricsText = $('#lyrics');
     const playMusicButton = $('#play-music');
-    const musixmatch_key = "933db136de4c5690257d41e434ec1143";
-    const youtube_key = "AIzaSyBVQsjnNwpI-fOih0uJq-n1KCb1WJTvmh8";
+    const musixMatchKey = "933db136de4c5690257d41e434ec1143";
+    const youtubeKey = "AIzaSyBVQsjnNwpI-fOih0uJq-n1KCb1WJTvmh8";
 
 })
